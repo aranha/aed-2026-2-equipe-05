2d360bed6665f4c4cf4389f939ba673ed6ef5881
255171 <pauloaranhadev@gmail.com>
2026-08-14 23:28:14 -0300

    first commit

---
+++ src/main/java/com/aed20262equipe05/Aed20262Equipe05Application.java
@@ -0,0 +1,13 @@
+package com.aed20262equipe05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aed20262Equipe05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aed20262Equipe05Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/aed20262equipe05/Aed20262Equipe05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aed20262equipe05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aed20262Equipe05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
